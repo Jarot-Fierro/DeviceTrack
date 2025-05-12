e5dea4d3018813f42ece297c1dec201ee2477e6b
Jarot-Fierro <140451417+Jarot-Fierro@users.noreply.github.com>
2025-05-12 14:55:24 -0400

    Updated Logical Save_History, order in templates

--- devicetrack/brand/views.py
+++ devicetrack/brand/views.py
@@ -9,6 +9,21 @@ from .models import Brand, BrandHistory
 from .forms import FormBrand
 
 
+def save_history(request, instance, old_instance, action):
+    model_name = instance.__class__.__name__
+
+    data = {
+        'id_brand': str(instance.id_brand),
+        'operation': action,
+        'old_data': old_instance if old_instance else None,
+        'new_data': model_to_dict(instance),
+        'user_login_history': request.user.username if request.user.username else 'anonymous',
+        'user_login': request.user if request.user.is_authenticated else None
+    }
+    if model_name == 'Brand':
+        BrandHistory.objects.create(**data)
+
+
 class BaseBrandFormView(TemplateView, FormMixin):
     form_class = FormBrand
     template_name = 'brand_list.html'
@@ -27,59 +42,13 @@ class BaseBrandFormView(TemplateView, FormMixin):
 
         return context
 
-    def save_history(self, instance, action):
-        request = self.request
-
-        data = model_to_dict(instance)  # Toda la instancia actual
-
-        if action == 'Create':
-            BrandHistory.objects.create(
-                id_brand=instance.id_brand,
-                operation=action,
-                old_data=None,
-                new_data=data,
-                user_login_history=request.user.username,
-                user_login=request.user
-            )
-        # elif action == 'update':
-        #     old_instance = Brand.objects.get(pk=instance.pk)
-        #     old_data = model_to_dict(old_instance)
-        #     new_data = data
-        #
-        #     # Solo guardamos los campos que cambiaron
-        #     changed_data = {
-        #         field: new_data[field]
-        #         for field in new_data
-        #         if old_data.get(field) != new_data[field]
-        #     }
-        #
-        #     if changed_data:
-        #         BrandHistory.objects.create(
-        #             brand=instance,
-        #             operation=action,
-        #             old_data=old_data,
-        #             new_data=changed_data,
-        #             user_login_history=request.user if request.user.is_authenticated else None
-        #         )
-        # elif action == 'toggle':
-        #     old_status = getattr(instance, 'status', '')
-        #     new_status = 'INACTIVE' if old_status == 'ACTIVE' else 'ACTIVE'
-        #
-        #     BrandHistory.objects.create(
-        #         brand=instance,
-        #         operation=action,
-        #         old_data={'status': old_status},
-        #         new_data={'status': new_status},
-        #         user_login_history=request.user if request.user.is_authenticated else None
-        #     )
-
 
 class BrandCreateView(BaseBrandFormView, ProcessFormView):
     def post(self, request, *args, **kwargs):
         form = self.get_form()
         if form.is_valid():
             messages.success(request, 'El registro a sido creado correctamente')
-            self.save_history(form.instance, 'create')
+            save_history(request, form.instance, None, 'create')
             return self.form_valid(form)
         else:
             return self.form_invalid(form)
@@ -101,10 +70,12 @@ class BrandUpdateView(BaseBrandFormView, ProcessFormView):
 
     def post(self, request, *args, **kwargs):
         form = self.get_form()
+        old_instance = model_to_dict(form.instance)
+
         if form.is_valid():
             if form.has_changed():
                 messages.success(request, 'El registro fue actualizado correctamente')
-                self.save_history(form.instance, 'update')
+                save_history(request, form.instance, old_instance, 'update')
                 return self.form_valid(form)
             else:
                 messages.info(request, 'No hubo cambios en el registro')
@@ -124,9 +95,14 @@ class BrandUpdateView(BaseBrandFormView, ProcessFormView):
 
 class BrandToggleStatusView(View):
     def get(self, request, pk):
-        brand = get_object_or_404(Brand, pk=pk)
-        brand.status = 'INACTIVE' if brand.status == 'ACTIVE' else 'ACTIVE'
-        brand.save(update_fields=['status', 'updated_at'])
+        instance = get_object_or_404(Brand, pk=pk)
+        old_instance = model_to_dict(instance)
+
+        instance.status = 'INACTIVE' if instance.status == 'ACTIVE' else 'ACTIVE'
+        instance.save(update_fields=['status', 'updated_at'])
+
+        save_history(request, instance, old_instance, 'toggle')
+
         messages.success(request, 'El estado de la marca fue actualizada correctamente')
         return redirect('brand_list')
 
@@ -134,3 +110,6 @@ class BrandToggleStatusView(View):
 class BrandHistoryView(ListView):
     model = BrandHistory
     template_name = 'brand_list_history.html'
+
+    def get_queryset(self):
+        return BrandHistory.objects.all().order_by('-updated_at')
