2dc070652fe6d61b13609a118640e29a0813c7f7
Jarot-Fierro <140451417+Jarot-Fierro@users.noreply.github.com>
2025-06-23 22:21:49 -0400

    Updated FBV in index to CBV, updated template

--- devicetrack/core/views.py
+++ devicetrack/core/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-from django.views.decorators.cache import never_cache
+from django.views.generic import ListView
+
+from transaction.models import DetailTransaction
 
 
-@never_cache
-def index(request):
-    return render(request, 'index.html')
+class CoreView(ListView):
+    template_name = 'index.html'
+    context_object_name = 'object_list'
+
+    def get_queryset(self):
+        queryset = DetailTransaction.objects.all().order_by('-created_at')
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Lista de Transacciones'
+        return context
--- devicetrack/devicetrack/urls.py
+++ devicetrack/devicetrack/urls.py
@@ -17,11 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from core import views
+from core.views import CoreView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('', CoreView.as_view(), name='index'),
     path('usuarios/', include('user.urls')),
     path('marcas/', include('brand.urls')),
     path('categorias/', include('category.urls')),
